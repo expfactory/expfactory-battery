49b5ac8d8bfc8c3d11e62f6f928d343ae69e3675
ian <ianeisenberg90@gmail.com>
2016-11-07 19:25:07 -0800

    updated key choices

--- static/js/utils/poldrack_fmri_utils.js
+++ static/js/utils/poldrack_fmri_utils.js
@@ -75,9 +75,8 @@ var create_trigger_block = function(trigger) {
 
 // test response keys
 var create_key_test_block = function(choice) {
-	var keycode_lookup = {'B': 'thumb', 'Y': 'index finger', 'G': 'middle finger', 
-					'R': 'ring finger', 'M': 'pinky', '1': 'thumb', '2': 'index finger', 
-					'3': 'middle finger', '4': 'ring finger', '5': 'pinky'}
+	var keycode_lookup = {'B': 'right', 'Y': 'bottom', 'G': 'right', 
+					'R': 'up', 'M': 'center'}
 	var finger = keycode_lookup[String.fromCharCode(choice)]
 	var instruct_text = "Please press your " + finger
 	if (finger == null) {
